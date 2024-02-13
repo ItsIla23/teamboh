18eb5457cc02906a2dcac04850fa8426255cc4bf
ItsIla23 <ipoziello23@fed.idserve.net>
2024-02-13 11:35:02 +0100

    first commit

---
+++ ProvaAppUITests/ProvaAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ProvaAppUITestsLaunchTests.swift
+//  ProvaAppUITests
+//
+//  Created by Ilaria Poziello on 13/02/24.
+//
+
+import XCTest
+
+final class ProvaAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
